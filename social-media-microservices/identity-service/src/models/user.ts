904a2d08ea6d59b3428f1b00ad14b1c24f989c0e
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-05-05 22:05:26 +0100

    identity service

--- social-media-microservices/identity-service/src/models/user.ts
+++ social-media-microservices/identity-service/src/models/user.ts
@@ -3,7 +3,29 @@ import argon2 from "argon2"
 
 const userSchema = new mongoose.Schema({
   username: {
-    type: string,
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  }
+  full_name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  }
+  profile_picture: {
+    type: String,
+  }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  }
+  password: {
+    type: String,
     required: true
   }
 })
